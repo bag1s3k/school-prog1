46d6daca7dec6704ac150b0dc50a433a8bf1000f
bag1s3k <jetam.mravenec@gmail.com>
2025-09-26 09:39:27 +0200

    oop basics

---
+++ OOPBasic/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPBasic;
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        CLovek clovek = new CLovek("Karel", 200, 100, 20);
+        
+        Console.WriteLine(clovek.getJmeno());
+        clovek.setJmeno("Maty");
+        Console.WriteLine(clovek.getJmeno());
+        Console.WriteLine(clovek.vaha);
+    }
+}
