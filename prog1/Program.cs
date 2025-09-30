37340f6a15fbae1b73b608e05a8c98b673ebe77f
bag1s3k <jetam.mravenec@gmail.com>
2025-09-30 15:26:06 +0200

    UPDATE .gitignore

---
+++ prog1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OOPBasic;
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        MyslimCislo(4);
+        MyslimCislo(10);
+        MyslimCislo();
+
+        int a = vypis<int>(5);
+        double b = vypis<double>(5.4);
+    }
+
+    static void MyslimCislo(int cislo = 0) {
+        Console.WriteLine($"Myslim cislo: {cislo}");
+    }
+
+    static T vypis<T>(T a) {
+        Console.WriteLine($"Vracím ti {a}");
+        return a;
+    }
+}
