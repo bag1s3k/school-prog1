c173f53b5228a4ba69f1e3adc2b7775cadacf59d
bag1s3k <jetam.mravenec@gmail.com>
2025-10-10 09:06:44 +0200

    clear workspace

--- prog1/Program.cs
+++ prog1/Program.cs
@@ -4,9 +4,6 @@ using System;
 
 class Program {
     static void Main(string[] args) {
-        Zlomek zlomek1 = new Zlomek(10, 5);
-        Zlomek zlomek2 = new Zlomek(6, 3);
-        
-        Console.WriteLine(zlomek1 * zlomek2);
+
     }
 }
