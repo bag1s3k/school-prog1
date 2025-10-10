fef5ec2e19d423b56f12b1220fc8369d195c1c05
bag1s3k <jetam.mravenec@gmail.com>
2025-10-10 09:06:12 +0200

    merge --squash

--- prog1/Program.cs
+++ prog1/Program.cs
@@ -4,31 +4,9 @@ using System;
 
 class Program {
     static void Main(string[] args) {
-        MyslimCislo(4);
-        MyslimCislo(10);
-        MyslimCislo();
-
-        int a = vypis<int>(5);
-        double b = vypis<double>(5.4);
-
-        int x = soucet(4, 5);
-        double y = soucet(1.2, 3.4, 5.8); // rozlisi podlep poctu parametru
-    }
-
-    static void MyslimCislo(int cislo = 0) {
-        Console.WriteLine($"Myslim cislo: {cislo}");
-    }
-
-    static T vypis<T>(T a) {
-        Console.WriteLine($"Vracím ti {a}");
-        return a;
-    }
-
-    static int soucet(int a, int b) {
-        return a + b;
-    }
-
-    static double soucet(double a, double b, double c) {
-        return a + b + c;
+        Zlomek zlomek1 = new Zlomek(10, 5);
+        Zlomek zlomek2 = new Zlomek(6, 3);
+        
+        Console.WriteLine(zlomek1 * zlomek2);
     }
 }
---
+++ prog1/Zlomek.cs
@@ -0,0 +1,39 @@
+﻿namespace prog1;
+
+internal class Zlomek {
+    public int citatel;
+    public int jmenovatel;
+
+    public Zlomek(int citatel, int b) {
+        this.citatel = citatel;
+        this.jmenovatel = (b == 0 ? 1 : b);
+    }
+
+    public static Zlomek operator *(Zlomek a, Zlomek b) {
+        return new Zlomek(a.citatel * b.citatel, a.jmenovatel * b.jmenovatel);
+    }
+
+    public static Zlomek operator /(Zlomek a, Zlomek b) {
+        // return new Zlomek(a.citatel * b.jmenovatel, a.jmenovatel * b.citatel);
+        return a * new Zlomek(b.jmenovatel, b.citatel);
+    }
+
+    public static Zlomek operator -(Zlomek a) {
+        return new Zlomek(-a.citatel, a.jmenovatel);
+    }
+    
+    public static Zlomek operator +(Zlomek a, Zlomek b) {
+        return new Zlomek(
+            a.citatel * b.jmenovatel + b.citatel * a.jmenovatel,
+            a.jmenovatel * b.jmenovatel
+            );
+    }
+
+    public static Zlomek operator -(Zlomek a, Zlomek b) {
+        return a + (-b);
+    }
+
+    public override string ToString() {
+        return $"({citatel}/{jmenovatel})";
+    }
+}
