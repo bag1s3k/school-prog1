16f5cc31947b809dd7e486770e65d78aca62c5b4
bag1s3k <jetam.mravenec@gmail.com>
2025-12-05 10:45:05 +0100

    05.12.2025 class work

---
+++ py/main.py
@@ -0,0 +1,73 @@
+﻿# Absolut value
+n = float(input("Enter num: "))
+if n >= 0:
+    print(n)
+else:
+    print(n * -1)
+
+# Even or Odd
+n = int(input("Enter int: "))
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Positive, Negative or Zero
+n = float(input("Enter num: "))
+if n > 0:
+    print("Positive")
+elif n == 0:
+    print("Zero")
+else:
+    print("Negative")
+
+# Is value in interval?
+array = input("Enter interval e.g. (1 5]: ")
+n = float(input("Enter num: "))
+
+nums = array[1:-1].split(" ")
+
+if array[0] == "[" and array[-1] == "]":
+    if float(nums[0]) <= n <= float(nums[1]):
+        print("In")
+    elif n < float(nums[0]):
+        print("Out below")
+    else:
+        print("Out above")
+
+elif array[0] == "(" and array[-1] == ")":
+    if float(nums[0]) < n < float(nums[1]):
+        print("In")
+    elif n <= float(nums[0]):
+        print("Out below")
+    else:
+        print("Out above")
+
+elif array[0] == "[" and array[-1] == ")":
+    if float(nums[0]) <= n < float(nums[1]):
+        print("In")
+    elif n < float(nums[0]):
+        print("Out below")
+    else:
+        print("Out above")
+
+elif array[0] == "(" and array[-1] == "]":
+    if float(nums[0]) < n <= float(nums[1]):
+        print("In")
+    elif n <= float(nums[0]):
+        print("Out below")
+    else:
+        print("Out above")
+
+# Which season
+n = int(input("Enter month (int): "))
+if 3 <= n <= 5:
+    print("Spring")
+elif 6 <= n <= 8:
+    print("Summer")
+elif 9 <= n <= 11:
+    print("Fall")
+elif 1 <= n <= 12:
+    print("Winter")
+else:
+    print("Doesn't exists")
